7a4db5cd888e5759ad630d93a08a09f9cf3534a7
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-10 21:54:55 +0530

    Cleanup generated content Logo image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        logo: "url('../public/logo/dv_stash_logo_white.png')",
       },
       colors: {
         od_red: '#e06c75',

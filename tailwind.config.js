5f326a3960831333de21b9ea2c48dac8f77bdb4d
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-10 15:37:04 +0530

    Add firacode font Setup onedark colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        od_red: '#e06c75',
+        od_green: '#98c379',
+        od_yellow: '#e5c07b',
+        od_blue: '#61afef',
+        od_purple: '#c678dd',
+        od_pink: '#f472b6',
+        od_cyan: '#56b6c2',
+        od_black: '#282c34',
+        od_gray: '#abb2bf',
+      },
     },
   },
   plugins: [],

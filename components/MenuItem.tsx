ad6d01998330bc682c6a30c8043f835bc893374e
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-14 21:08:36 +0530

    Headless UI lib User Menu

---
+++ components/MenuItem.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+type MenuItemProps = {
+  title: string
+  icon: any
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ title, icon: Icon }) => {
+  return (
+    <div className="flex cursor-pointer gap-2 p-2 text-od_gray hover:bg-od_blue/40">
+      <div className="flex h-6 w-6 items-center justify-center">
+        <Icon className="h-5 w-5 " />
+      </div>
+      <div className="mr-2 flex-1">{title}</div>
+    </div>
+  )
+}
+
+export default MenuItem
--- components/User.tsx
+++ components/User.tsx
@@ -1,14 +1,34 @@
 'use client'
 
-import { UserCircleIcon } from '@heroicons/react/24/outline'
+import { Menu } from '@headlessui/react'
+import {
+  ArrowDownOnSquareIcon,
+  ArrowLeftOnRectangleIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline'
+import MenuItem from './MenuItem'
 
 type UserProps = {}
 
 const User: React.FC<UserProps> = ({}) => {
+  const menuItems = [
+    { title: 'Login', icon: ArrowLeftOnRectangleIcon },
+    { title: 'Register', icon: ArrowDownOnSquareIcon },
+  ]
+
   return (
-    <div className="group flex h-12 w-12 cursor-pointer items-center justify-center">
-      <UserCircleIcon className="h-6 w-6 text-od_gray group-hover:text-white" />
-    </div>
+    <Menu>
+      <Menu.Button className="group flex h-12 w-12 cursor-pointer items-center justify-center">
+        <UserCircleIcon className="h-6 w-6 text-od_gray group-hover:text-white" />
+      </Menu.Button>
+      <Menu.Items className="max-w absolute bottom-0 left-12 flex w-32 min-w-max max-w-xs flex-col bg-od_gray/5">
+        {menuItems.map((item, i) => (
+          <Menu.Item key={i}>
+            <MenuItem title={item.title} icon={item.icon}></MenuItem>
+          </Menu.Item>
+        ))}
+      </Menu.Items>
+    </Menu>
   )
 }
 

eddf7033a761a3f73d32de57a2b7c6742972b68a
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-16 21:12:02 +0530

    Testing sidebar edge cases

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -91,30 +91,114 @@ const SideBar: React.FC<SideBarProps> = ({}) => {
       icon: PencilSquareIcon,
       path: '/about',
     },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Products',
+      icon: ListBulletIcon,
+      path: '/products',
+    },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Projects',
+      icon: BoltIcon,
+      path: '/projects',
+    },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Services',
+      icon: CubeTransparentIcon,
+      path: '/services',
+    },
+    {
+      id: SideBarActions.ARTICLES,
+      title: 'News',
+      icon: NewspaperIcon,
+      path: '/news',
+    },
+    {
+      id: SideBarActions.ARTICLES,
+      title: 'Blog',
+      icon: BookmarkIcon,
+      path: '/blog',
+    },
+    {
+      id: SideBarActions.STORE,
+      title: 'Store',
+      icon: ShoppingCartIcon,
+      path: '/store',
+    },
+    {
+      id: SideBarActions.LOCAL,
+      title: 'Contact',
+      icon: PhoneIcon,
+      path: '/contact',
+    },
+    {
+      id: SideBarActions.LOCAL,
+      title: 'About',
+      icon: PencilSquareIcon,
+      path: '/about',
+    },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Products',
+      icon: ListBulletIcon,
+      path: '/products',
+    },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Projects',
+      icon: BoltIcon,
+      path: '/projects',
+    },
+    {
+      id: SideBarActions.SERVICES,
+      title: 'Services',
+      icon: CubeTransparentIcon,
+      path: '/services',
+    },
+    {
+      id: SideBarActions.ARTICLES,
+      title: 'News',
+      icon: NewspaperIcon,
+      path: '/news',
+    },
+    {
+      id: SideBarActions.ARTICLES,
+      title: 'Blog',
+      icon: BookmarkIcon,
+      path: '/blog',
+    },
+    {
+      id: SideBarActions.STORE,
+      title: 'Store',
+      icon: ShoppingCartIcon,
+      path: '/store',
+    },
+    {
+      id: SideBarActions.LOCAL,
+      title: 'Contact',
+      icon: PhoneIcon,
+      path: '/contact',
+    },
+    {
+      id: SideBarActions.LOCAL,
+      title: 'About',
+      icon: PencilSquareIcon,
+      path: '/about',
+    },
   ]
-
-  const handleActionClick = (action: SideBarAction) => () => {
-    router.push(action.path)
-
-    switch (action.id) {
-      case SideBarActions.HOME:
-      case SideBarActions.LOCAL:
-      case SideBarActions.ARTICLES:
-      case SideBarActions.SERVICES:
-        break
-    }
-  }
 
   return (
     <div className="z-20 flex h-screen w-12 flex-col bg-od_black_90">
       <Logo />
-      <div className="flex flex-1 flex-col">
-        <ul className="flex-1">
+      <div className="flex h-[calc(100vh-3rem)] flex-1 flex-col">
+        <ul className="h-[calc(100vh-6rem)] flex-1 touch-pan-y snap-y overflow-y-auto overflow-x-hidden scroll-smooth">
           {actions.map((action, i) => (
             <li
-              className="cursor-pointer"
               key={i}
-              onClick={handleActionClick(action)}>
+              className="cursor-pointer snap-start bg-od_black_90"
+              onClick={() => router.push(action.path)}>
               <SideBarIcon icon={action.icon} title={action.title} />
             </li>
           ))}

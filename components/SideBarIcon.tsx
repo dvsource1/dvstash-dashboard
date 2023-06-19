7f6cd1559b4175f4682910362f2ecdd8ee092e88
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-19 16:48:18 +0530

    Chnage side-bar-icon tooltip

--- components/SideBarIcon.tsx
+++ components/SideBarIcon.tsx
@@ -1,3 +1,10 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/Tooltip'
+
 type SideBarIconProps = {
   icon: any
   title: string
@@ -5,12 +12,18 @@ type SideBarIconProps = {
 
 const SideBarIcon: React.FC<SideBarIconProps> = ({ icon: Icon, title }) => {
   return (
-    <div className="group flex h-12 w-12 items-center justify-center hover:bg-od_black_90__blue_40">
-      <Icon className="h-6 w-6 text-od_gray group-hover:text-white"></Icon>
-      <span className="absolute left-12 scale-0 bg-od_black__yellow_20 px-2 py-1 text-xs font-semibold text-od_black__blue_80 group-hover:scale-100">
-        {title}
-      </span>
-    </div>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="group flex h-12 w-12 items-center justify-center hover:bg-od_black_90__blue_40">
+            <Icon className="h-6 w-6 text-od_gray group-hover:text-white"></Icon>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          <p>{title}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 

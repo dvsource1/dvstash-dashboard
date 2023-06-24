3078b2b54d7c2fbf1ace05c8ab0f66ca2bd45e85
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-24 15:02:52 +0530

    Online status

--- components/StatusBar.tsx
+++ components/StatusBar.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+import { useOnlineStatus } from '@/hooks/use-online-status'
 import { useClerk, useUser } from '@clerk/nextjs'
-import { PlusIcon } from '@heroicons/react/24/outline'
+import { PlusIcon, WifiIcon } from '@heroicons/react/24/outline'
 import { useRouter } from 'next/navigation'
 import User from './User'
 
@@ -11,6 +12,8 @@ const StatusBar: React.FC<StatusBarProps> = ({}) => {
   const { isLoaded, isSignedIn, user } = useUser()
   const { signOut } = useClerk()
   const router = useRouter()
+
+  const isOnline = useOnlineStatus()
 
   const loggedInUserActions = [
     { title: 'Logout', action: () => signOut() },
@@ -23,35 +26,42 @@ const StatusBar: React.FC<StatusBarProps> = ({}) => {
   ]
 
   return (
-    <div
-      className={`${
-        isSignedIn ? 'bg-OD_RED-800' : 'bg-OD_BLACK'
-      } flex h-4 justify-between border-t border-OD_GRAY-400 text-xxs`}>
-      {isLoaded && isSignedIn && user ? (
-        <User
-          isLoaded={isLoaded}
-          isSignedIn={isSignedIn}
-          user={user}
-          signOut={signOut}
-        />
-      ) : (
-        <div className="flex items-center px-2 text-OD_GRAY">
-          <p>&copy; Copyright 2023 | DVSTASH</p>
+    <div className={`${isSignedIn ? 'bg-OD_BLACK' : 'bg-OD_BLACK'} text-xxs`}>
+      <div className="flex h-4 flex-row-reverse justify-between border-t border-OD_GRAY-400">
+        <div className="flex flex-row items-center gap-1 px-4 text-OD_GRAY">
+          <WifiIcon
+            className={`${isOnline ? 'text-OD_GREEN' : 'text-OD_RED'} h-3 w-3`}
+          />
+          <p className={isOnline ? 'text-OD_GREEN' : 'text-OD_GRAY'}>Online</p>
         </div>
-      )}
-      <div className="flex flex-1 flex-row-reverse gap-2 text-OD_GRAY-600">
-        {(isLoaded && isSignedIn
-          ? loggedInUserActions
-          : loggedOutUserActions
-        ).map((action, i) => (
-          <div
-            className="group flex cursor-pointer items-center gap-1 border-l border-OD_GRAY-400 pl-2 pr-4"
-            key={i}
-            onClick={action.action}>
-            <PlusIcon className="h-2 w-2" />
-            <p className="group-hover:underline">{action.title}</p>
+      </div>
+      <div className="flex h-4 justify-between border-t border-OD_GRAY-400">
+        {isLoaded && isSignedIn && user ? (
+          <User
+            isLoaded={isLoaded}
+            isSignedIn={isSignedIn}
+            user={user}
+            signOut={signOut}
+          />
+        ) : (
+          <div className="flex items-center px-2 text-OD_GRAY">
+            <p>&copy; Copyright 2023 | DVSTASH</p>
           </div>
-        ))}
+        )}
+        <div className="flex flex-1 flex-row-reverse gap-2 text-OD_GRAY-600">
+          {(isLoaded && isSignedIn
+            ? loggedInUserActions
+            : loggedOutUserActions
+          ).map((action, i) => (
+            <div
+              className="group flex cursor-pointer items-center gap-1 border-l border-OD_GRAY-400 pl-2 pr-4"
+              key={i}
+              onClick={action.action}>
+              <PlusIcon className="h-2 w-2" />
+              <p className="group-hover:underline">{action.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )

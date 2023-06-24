559a6d0e65286ff40102530d5fe8a0c1bae5d058
Viraj Kaushalye <virajkaush@gmail.com>
2023-06-24 18:01:07 +0530

    Privacy Policy & Terms & Conditions pages

--- app/(SEO)/(local)/privacy-policy/page.tsx
+++ app/(SEO)/(local)/privacy-policy/page.tsx
@@ -6,7 +6,149 @@ const Page: React.FC<PageProps> = ({}) => {
   return (
     <div className="flex h-full flex-col gap-2">
       <Header title={'Privacy Policy'} />
-      <div className="overflow-auto"></div>
+      <div className="mb-4 flex flex-col gap-6 overflow-auto p-4 text-OD_GRAY">
+        <h2 className="font-bold">Effective Date: 2023 June 20</h2>
+        <p className="text-justify">
+          This Privacy Policy outlines the practices and policies implemented by
+          DVStash (&quot;we&quot;, &quot;us&quot; or &quot;our&quot;) regarding
+          the collection, use, and disclosure of personal information when you
+          visit our website or use our services. We are committed to protecting
+          your privacy and ensuring the security of the personal information you
+          provide to us. By accessing our website or using our services, you
+          consent to the practices described in this Privacy Policy.
+        </p>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">1. Information We Collect</h3>
+
+          <p className="text-justify">
+            We may collect personal information that you voluntarily provide to
+            us, such as your name, email address, phone number, and any other
+            information you choose to provide. Additionally, we may
+            automatically collect certain information, including your IP
+            address, browser type, operating system, and referring website.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">2. Use of Information</h3>
+          <p className="text-justify">
+            We may use the collected information for various purposes, including
+            but not limited to:
+          </p>
+
+          <ul className="list-disc pl-4">
+            <li>
+              Providing and personalizing our services to meet your specific
+              needs.
+            </li>
+            <li>
+              Responding to your inquiries, requests, or customer
+              service-related matters.
+            </li>
+            <li>
+              Sending you important updates, newsletters, or marketing
+              communications.
+            </li>
+            <li>
+              Analyzing and improving the effectiveness of our website and
+              services.
+            </li>
+            <li>
+              Protecting our rights, property, or safety, as well as the rights,
+              property, and safety of our users or others.
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">3. Information Sharing</h3>
+          <p className="text-justify">
+            We may share your personal information with trusted third parties
+            who assist us in providing our services, such as website hosting,
+            data analysis, and customer support. These third parties are
+            obligated to maintain the confidentiality and security of your
+            information and are prohibited from using your information for any
+            other purposes.
+          </p>
+          <p className="text-justify">
+            We may also disclose your personal information when required by law,
+            such as in response to a court order or governmental request, or if
+            we believe it is necessary to protect our rights, property, or
+            safety.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">4. Data Security</h3>
+          <p className="text-justify">
+            We take reasonable precautions to protect your personal information
+            from unauthorized access, use, or disclosure. However, no method of
+            transmission over the internet or electronic storage is 100% secure.
+            Therefore, while we strive to protect your information, we cannot
+            guarantee its absolute security.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">
+            5. Cookies and Tracking Technologies
+          </h3>
+          <p className="text-justify">
+            We may use cookies and similar tracking technologies to enhance your
+            experience on our website. These technologies allow us to collect
+            information about your browsing activities, such as pages visited
+            and links clicked, to personalize your experience and deliver
+            relevant content. You can choose to disable cookies through your
+            browser settings; however, this may limit certain functionalities of
+            our website.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">6. Third-Party Links</h3>
+          <p className="text-justify">
+            Our website may contain links to third-party websites or services
+            that are not controlled or operated by us. This Privacy Policy does
+            not apply to those third-party websites or services. We recommend
+            reviewing the privacy policies of those websites or services before
+            providing any personal information.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">7. Children&apos;s Privacy</h3>
+          <p className="text-justify">
+            Our website and services are not directed to individuals under the
+            age of 13. We do not knowingly collect personal information from
+            children. If you are a parent or guardian and believe that your
+            child has provided us with personal information, please contact us,
+            and we will promptly remove such information from our records.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">8. Updates to Privacy Policy</h3>
+          <p className="text-justify">
+            We reserve the right to update or modify this Privacy Policy at any
+            time. We will notify you of any changes by posting the revised
+            Privacy Policy on our website. The updated Privacy Policy will be
+            effective as of the date of posting.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">9. Contact Us</h3>
+          <p className="text-justify">
+            If you have any questions, concerns, or requests regarding this
+            Privacy Policy or the handling of your personal information, please
+            contact us at virajkaush@gmail.com.
+          </p>
+        </div>
+
+        <p className="text-justify">
+          By using our website or services, you acknowledge that you have read
+          and understood this Privacy Policy and agree to its terms.
+        </p>
+
+        <address>
+          <p className="text-justify">DVStash</p>
+          <p className="text-justify">Earch</p>
+        </address>
+      </div>
     </div>
   )
 }
--- app/(SEO)/(local)/terms-and-conditions/page.tsx
+++ app/(SEO)/(local)/terms-and-conditions/page.tsx
@@ -6,7 +6,87 @@ const Page: React.FC<PageProps> = ({}) => {
   return (
     <div className="flex h-full flex-col gap-2">
       <Header title={'Terms & Conditions'} />
-      <div className="overflow-auto"></div>
+      <div className="mb-4 flex flex-col gap-6 overflow-auto p-4 text-OD_GRAY">
+        <h2 className="font-bold">Effective Date: 2023 June 20</h2>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">1. Acceptance of Terms</h3>
+          <p className="text-justify">
+            By accessing or using this website, you agree to be bound by these
+            Terms and Conditions. If you do not agree with any part of these
+            terms, please do not use this website.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">2. Intellectual Property</h3>
+          <p className="text-justify">
+            All content on this website, including text, graphics, logos,
+            images, and software, is the property of [Your Company Name] and
+            protected by intellectual property laws. You may not reproduce,
+            distribute, modify, or use any content without our prior written
+            consent.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">3. User Conduct</h3>
+          <p className="text-justify">
+            When using this website, you agree to abide by the following rules:
+          </p>
+          <ul>
+            <li>Do not violate any applicable laws or regulations.</li>
+            <li>Do not infringe upon the rights of others.</li>
+            <li>
+              Do not upload or distribute any harmful or offensive content.
+            </li>
+            <li>
+              Do not interfere with the operation of this website or its
+              underlying technology.
+            </li>
+            <li>
+              Do not attempt to gain unauthorized access to any part of this
+              website.
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">4. Limitation of Liability</h3>
+          <p className="text-justify">
+            [Your Company Name] shall not be liable for any direct, indirect,
+            incidental, consequential, or punitive damages arising out of your
+            access or use of this website. This includes any damages resulting
+            from errors, omissions, interruptions, or delays in operation.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">5. Links to Third-Party Websites</h3>
+          <p className="text-justify">
+            This website may contain links to third-party websites. These links
+            are provided solely for your convenience and do not imply
+            endorsement or responsibility for the content or activities of those
+            websites.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">6. Termination</h3>
+          <p className="text-justify">
+            We reserve the right to terminate or suspend your access to this
+            website at any time, without prior notice, for any reason, including
+            if we believe you have violated these Terms and Conditions.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold">7. Governing Law</h3>
+          <p className="text-justify">
+            These Terms and Conditions shall be governed by and construed in
+            accordance with the laws of [Your Country].
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
